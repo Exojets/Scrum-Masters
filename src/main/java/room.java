4be0b1ab1ed06121b6af1636f7cef4a16470459d
Glen Roe <roeglen@gmail.com>
2022-04-02 18:05:02 -0700

    for abraham

---
+++ src/main/java/room.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Room {
+   public static void main(String[] args) {
+      
+      String amenities = "room";
+      
+      int cost = 1;
+      int size = 1;
+      int availability = 1;
+      
+      
+     
