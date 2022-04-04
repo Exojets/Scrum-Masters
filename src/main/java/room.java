632390515d1e919619cafd762fc97eead0b93437
Abraham Balyan <abraham.balyan.105@my.csun.edu>
2022-04-04 14:47:50 -0700

    change room file

--- src/main/java/room.java
+++ src/main/java/room.java
@@ -1,14 +1,68 @@
+
+
 import java.io.File;
 import java.util.Scanner;
 
 public class Room {
-   public static void main(String[] args) {
+   private String amenities;
+   private int cost;
+   private int size;
+   private int availability;
+
+   
+   public Room(String name) throws Exception {
       
-      String amenities = "room";
+      private Scanner x;
+   
+      public void openFile(){
+         try{
+            x = new Scanner(new File("Room1.txt"));
+            x = new Scanner(new File("Room2.txt"));
+            x = new Scanner(new File("Room3.txt"));
+         }
+      }
+         
+         
+   public void readFile();
+        
+        
+   String amenities = "room";
       
-      int cost = 1;
-      int size = 1;
-      int availability = 1;
+   int cost = 1;
+   int size = 1;
+   int availability = 1;
+      
+      
+   public String costGet(){ 
+      return cost;
+   }
+      
+   public String sizeGet(){
+      return size;
+   }
+      
+   public String amenitiesGet(){
+      return amenities;
+   }
+      
+   public void amenitiesSet(String newAmenities){
+      amenities = newAmenities;
+   }
+      
+   public String availabilityGet(){
+      return availability;
+   }
+      
+   public void availabilitySet(String newAvailability){
+      availability = newAvailability;
+   }
+         
+      
+      
+      
+      
+      
+      
       
       
      
