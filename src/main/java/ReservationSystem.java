b21dfe8a242e01d63b40571ebe94c0a65c2bfe5c
ExoJets <RyPK94@gmail.com>
2022-04-20 15:53:59 -0700

    Added cancelReservation and changeReservation

--- src/main/java/ReservationSystem.java
+++ src/main/java/ReservationSystem.java
@@ -33,14 +33,14 @@ public class ReservationSystem {
 
      //account will be a string
      //room would be 2 int for month and for day
-    public String makeReservation(Room r, int month, int day, int roomNumber) throws Exception{
-        roomTemp=r;
+    public String makeReservation(Room RoomReserve , int month, int day, int roomNumber) throws Exception{
+        roomTemp=RoomReserve;
         String returnValue = "No Reservation Made"; // no reservation has been made
        if (roomTemp.availabilityGet(month , day) > 0){
            int MonthPrint=month+1;
            int DayPrint=day+1;
-            accountTemp.reservationsSet(accountTemp.reservationsGet()+ " " + MonthPrint +" " + DayPrint+" " + roomNumber +"");
-            accountTemp.notificationsSet(accountTemp.notificationsGet()+ "There is a new notification! -");
+            accountTemp.reservationsSet(accountTemp.reservationsGet() + MonthPrint +" " + DayPrint+" " + roomNumber +"-");
+            accountTemp.notificationsSet(accountTemp.notificationsGet()+ "There is a new notification!");
             
             // access the user and room files
             File userfile = new File((accountTemp.usernameGet()+".txt")); 
@@ -58,7 +58,6 @@ public class ReservationSystem {
             writeUserFile.write(accountTemp.passwordGet()+"\n"); 
             writeUserFile.write(accountTemp.reservationsGet()+"\n");
             writeUserFile.write(accountTemp.notificationsGet()+"\n");
-            writeUserFile.write(accountTemp.managerFlagGet().toString());
             writeUserFile.close();
         
            //write to the new room file
@@ -79,9 +78,108 @@ public class ReservationSystem {
     return returnValue;
     }
        
+    public String cancelReservation(Room RoomCancel, int month, int day, int RoomNumber) throws Exception{
+        roomTemp = RoomCancel;
+        String returnValue;
+        String reconstruction = "";
+        String target = "";
+        String temp;
+        boolean cancelled = false;
+        
+        target = target.concat(Integer.toString(month + 1)+" ");
+        target = target.concat(Integer.toString(day +1)+" ");
+        target = target.concat(Integer.toString(RoomNumber));
+        
+        
+        
+        Scanner scanner = new Scanner(accountTemp.reservationsGet()); 
+        scanner.useDelimiter("-");
+        while(scanner.hasNext() ){
+        temp = scanner.next();
+        if (!target.equals(temp)||(cancelled==true)){
+            reconstruction = reconstruction.concat(temp + "-");
+        }
+        else{
+            cancelled = true;
+        }
+        
+        }
+       
+        if ( cancelled == false)
+        returnValue = "Reservation Does not exist!";
+        else {
+        returnValue ="Reservation cancelled"; 
+        accountTemp.notificationsSet(accountTemp.notificationsGet()+ "There is a new notification!");
+        accountTemp.reservationsSet(reconstruction);
+        
+        File userfile = new File((accountTemp.usernameGet()+".txt"));
+        FileWriter writeUserFile = new FileWriter(accountTemp.usernameGet()+".txt");
+        
+        File roomfile = new File ("Room"+RoomNumber+".txt");
+        FileWriter writeRoomFile = new FileWriter("Room"+RoomNumber+".txt");
+        
+         userfile.delete(); 
+         userfile.createNewFile();
+         roomfile.delete();
+         roomfile.createNewFile();
+            
+         writeUserFile.write(accountTemp.passwordGet()+"\n"); 
+         writeUserFile.write(accountTemp.reservationsGet()+"\n");
+         writeUserFile.write(accountTemp.notificationsGet()+"\n");
+         writeUserFile.close();
+         
+         writeRoomFile.write(roomTemp.costGet()+"\n");
+         writeRoomFile.write(roomTemp.amenitiesGet()+"\n");
+        
+        
+        
+         
+          roomTemp.availabilitySet(month,day, 1); 
+        
+            for (int monthIndex = 0; monthIndex < 12 ;monthIndex++) {
+                for (int dayIndex = 0; dayIndex < 31; dayIndex++){
+                    writeRoomFile.write(roomTemp.availabilityGet(monthIndex,dayIndex)+" ");
+                }
+            }
+            writeRoomFile.close();
+            } 
+            
+            return returnValue;
+    }
     
-    /* user text password, reservation, notifications
-    room text cost amenities, availabilities
-    */
-
+    public String changeReservation (Room RoomCancel, Room RoomReserve, int MonthCancel, int MonthReserve, int DayCancel, int DayReserve, int RoomNumberCancel, int RoomNumberReserve) throws Exception {
+        
+        String target = "";
+        String temp;
+        Boolean reservationExists = false;
+        String returnValue;
+        
+        target = target.concat(Integer.toString(MonthCancel + 1)+" ");
+        target = target.concat(Integer.toString(DayCancel +1)+" ");
+        target = target.concat(Integer.toString(RoomNumberCancel));
+        
+        Scanner scanner = new Scanner(accountTemp.reservationsGet()); 
+        scanner.useDelimiter("-");
+        while(scanner.hasNext() ){
+        temp = scanner.next();
+            if (target.equals(temp)){
+                reservationExists = true;
+         
+            }
+        }
+        if (reservationExists == true){
+            roomTemp = RoomReserve;
+            if (roomTemp.availabilityGet(MonthReserve , DayReserve) > 0) {
+              cancelReservation(RoomCancel, MonthCancel,DayCancel,RoomNumberCancel);
+              makeReservation(RoomReserve,MonthReserve,DayReserve,RoomNumberReserve);
+              returnValue = "Reservation successfully changed!";
+            }
+            else
+                returnValue = "No availablity for date selected";
+        }
+        else
+            returnValue = "Reservation does not exist!";
+        
+        return returnValue;
+    }
 }
